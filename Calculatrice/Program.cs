a5c5c1e8604452d6d4c5c550e95c0ed4b1ed3b37
Vikkett <144105889+Vikkett@users.noreply.github.com>
2023-09-05 10:23:49 +0200

    Initial commit

---
+++ Calculatrice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculatrcie");
+
+            Console.WriteLine();
+            Console.WriteLine("Write the first number");
+            string firstNum = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Choose the operators");
+            string operators = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Write the second number");
+            string secondNum = Console.ReadLine();
+           
+            string operations = Console.ReadLine();
+            int calculatrice = Convert.ToInt32(operators); 
+            switch (calculatrice)
+            {
+                case 1:
+                    Console.WriteLine("+");
+                    break;
+                case 2:
+                    Console.WriteLine("-");
+                    break;
+                case 3:
+                    Console.WriteLine("*");
+                    break;
+                case 4:
+                    Console.WriteLine("/");
+                    break;
+
+
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculatrice
+ projet preap
